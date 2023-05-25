4d82a910ebcf07bb60599aa7cc1bd4771479190f
Davi Costa <davifncosta@gmail.com>
2023-05-25 10:46:51 -0300

    finished cards

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,9 +3,11 @@ import AboutCards from "./AboutCards";
 const About = () => {
   return (
     <>
-     <div className="flex gap-10 -mt-[8.5rem] md1000:flex-col px-20">
-         <AboutCards />
-     </div>
+     <section className="flex flex-col justify-between gap-2 about-section pb-[20rem]">
+         <div className="flex gap-10 -mt-[8.5rem] md1000:flex-col px-20">
+             <AboutCards />
+         </div>
+     </section>
     </>
   );
 };
--- src/components/About/AboutCards.jsx
+++ src/components/About/AboutCards.jsx
@@ -4,8 +4,10 @@ const AboutCards = () => {
   return (
     <>
       {cardList.map((card, id) => (
-        <div key={card.id} className="flex flex-col cursor-pointer bg-[#37383A] justify-center py-6 px-10 text-center items-center mt-12 rounded-[32px] shadow-2xl min-h-[340px] w-full text-white md1000:min-h-[260px]">
-          <img src={card.img} alt="box_img" className="w-[75px] mb-4" />
+        <div key={card.id} className={`flex flex-col cursor-pointer bg-[#37383A] justify-center py-6 px-10 text-center items-center mt-12 rounded-[32px] shadow-2xl min-h-[340px] w-full text-white md1000:min-h-[260px]  ${id === 0 ? "card-item-div-0" :
+        id === 1 ? "card-item-div-1" : 
+        id === 2 ?"card-item-div-2" : "card-item-div-3" }`}>
+          <img src={card.img} alt="box_img" className="w-[75px] mb-4"/>
           <p className=" text-[24px] font-bold uppercase mb-7">{card.title}</p>
           <p className="text-[15px] font-medium leading-2 w-full">
             {card.description}
