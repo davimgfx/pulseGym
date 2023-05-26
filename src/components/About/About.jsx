d9caf080c15d440bf06bb206580ad1bbaaa8cd08
Davi Costa <davifncosta@gmail.com>
2023-05-26 14:05:53 -0300

    responsive about us

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -31,22 +31,22 @@ const About = () => {
                 program meticulously crafted to propel you towards your ultimate
                 fitness aspirations.
               </p>
-              <div className="flex gap-[2rem] text-[26px] mt-[4rem] mb-[4rem] min540:hidden md1200:items-center md1200:flex md1200:text-center">
+              <div className="flex gap-[2rem] text-[26px] mt-[4rem] mb-[4rem] min540:flex-col md1200:items-center md1200:flex md1200:text-center">
                 <div className="flex flex-col justify-center items-center gap-3">
-                  <i class="fa-solid fa-dumbbell text-[4.5rem] mb-[0.1rem] min800:text-[4rem]"></i>
+                  <i className="fa-solid fa-dumbbell text-[4.5rem] mb-[0.1rem] min800:text-[4rem]"></i>
                   <p className="text-[22px] text-center text-black font-bold min800:text-[20px] leading-tight">
                     Modern <br />
                     Equipaments
                   </p>
                 </div>
-                <div className="flex flex-col justify-center items-center border-solid border-l border-[rgb(0,0,0,0.2)] border-r px-[3.5rem] gap-[1.3rem] text-[26px] mb-[2rem] ">
-                  <i class="fa-solid fa-basketball text-[4.5rem] min800:text-[4rem]"></i>
+                <div className="flex flex-col justify-center items-center border-solid border-l border-[rgb(0,0,0,0.2)] min540:border-none border-r min540:mb-[0] px-[3.5rem] gap-[1.3rem]  min540:gap-[0.3rem] text-[26px] mb-[2rem] ">
+                  <i className="fa-solid fa-basketball text-[4.5rem] min800:text-[4rem]"></i>
                   <p className="text-[22px] text-center text-black font-bold min800:text-[20px]">
                     Sports
                   </p>
                 </div>
                 <div className="flex flex-col justify-center items-center gap-3">
-                  <i class="fa-regular fa-user text-[4.5rem] mb-[0.1rem] font-thin min800:text-[4rem]"></i>
+                  <i className="fa-solid fa-user text-[4.5rem] mb-[0.1rem] min800:text-[4rem]"></i>
                   <p className="text-[22px] text-center text-black font-bold min800:text-[20px] leading-tight">
                     Skilled <br />
                     professionals
