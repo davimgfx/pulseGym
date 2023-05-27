57278a9bf1fd9566c61927ad38a6f7be4d9eb2f1
Davi Costa <davifncosta@gmail.com>
2023-05-27 12:16:05 -0300

    starter the banner

--- src/components/Class/Class.jsx
+++ src/components/Class/Class.jsx
@@ -11,10 +11,11 @@ import {
 
 const Class = () => {
   return (
-    <section id="class" className="clear-both">
+    <>
+    <section id="class">
       <div className="container page-padding">
         <div>
-          <div className="flex flex-col text-center items-center relative ">
+          <div className="flex flex-col text-center items-center relative mb-[10rem]">
             <h2 className="text-black font-semibold text-[24px] relative uppercase z-10 pl-16 md1000:pl-0 ml-9 md1200:-ml-[2rem] md1000:ml-0 ">
               OUR CLASSES
             </h2>
@@ -83,21 +84,14 @@ const Class = () => {
                 </p>
               </div>
             </div>
-            <h2 className="text-black font-semibold text-[24px] relative uppercase z-10 pl-16 md1000:pl-0 ml-9 md1200:-ml-[2rem] md1000:ml-0 mt-[23rem]">
-              OUR CLASSES
-            </h2>
-            <img
-              src={titleBg}
-              alt="text_bg"
-              className="w-[25rem] absolute top-[-3px] translate-x-10 md1200:translate-x-0 min250:hidden "
-            />
-            <h2 className="font-bold text-[40px] uppercase min450:text-[32px] min620:text-[36px] hero-h1 mt-[2rem] md1200:mb-10">
-              We offer top-notch flexible classes.
-            </h2>
           </div>
         </div>
       </div>
     </section>
+    <div className="class-banner">
+
+    </div>
+    </>
   );
 };
 
